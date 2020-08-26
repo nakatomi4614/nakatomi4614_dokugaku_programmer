402c333af99becfa2bd92bde19d0b8ff77e340ff
nakatomi4614 <nakatomi4614@gmail.com>
2020-08-26 21:02:32 +0900

    比較演算子

--- python_chapter3.py
+++ python_chapter3.py
@@ -1,29 +1,17 @@
 #　一行目のコメントは入力省略しました
 
-#算術演算子
+#比較演算子 結果はbool
 
 A = 13
 B = 5
 
-answer = A // B
-print(answer)
-answer = A % B
-print(answer)
-A = 12
-B = 2
-answer = A % B
-print(answer)
-A = 11
-answer = A % B
-print(answer)
-A = 14
-B = 3
-answer = A // B
-print(answer)
-answer = A / B
-print(answer)
-print(2**3)
+print(A > B) #True
+print(A < B) #False
+print(2>=2)
+print(2<=2)
+print(2==2)
+print(1==2)
+print(1!=2) #not equalは!=
+print(2!=2)
 
-#優先順位は（）累乗掛け算割り算足し算引き算
-print(2 + 2 * 2)
-print((2 + 2) * 2)
+
