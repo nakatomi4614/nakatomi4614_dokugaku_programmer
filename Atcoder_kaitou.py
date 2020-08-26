1433f52f4e8e80eed9b776054a5df1ba7f735d4c
nakatomi4614 <nakatomi4614@gmail.com>
2020-08-26 21:51:56 +0900

    #https://atcoder.jp/contests/abc047/tasks/abc047_a

--- Atcoder_kaitou.py
+++ Atcoder_kaitou.py
@@ -1,1 +1,12 @@
+#数値を空白区切りで入力する
+S_list = list(map(int, input("入力:").split()))
+
+S_max = max(S_list)
+answer = (sum(S_list)) / S_max
+if answer == 2:
+    result = "Yes"
+else:
+    result = "No"
+print(result)
+
 
