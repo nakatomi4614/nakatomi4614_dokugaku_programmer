10e9b4f2e3b24508aece54ca8bb9716e81a8a0a7
nakatomi4614 <nakatomi4614@gmail.com>
2020-08-26 20:59:01 +0900

    算術演算子

--- python_chapter3.py
+++ python_chapter3.py
@@ -1,6 +1,29 @@
 #　一行目のコメントは入力省略しました
 
-y = 2
-    x =1
+#算術演算子
 
-    #IndentationError: unexpected indent
+A = 13
+B = 5
+
+answer = A // B
+print(answer)
+answer = A % B
+print(answer)
+A = 12
+B = 2
+answer = A % B
+print(answer)
+A = 11
+answer = A % B
+print(answer)
+A = 14
+B = 3
+answer = A // B
+print(answer)
+answer = A / B
+print(answer)
+print(2**3)
+
+#優先順位は（）累乗掛け算割り算足し算引き算
+print(2 + 2 * 2)
+print((2 + 2) * 2)
