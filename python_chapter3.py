b3aaa512a6035750e83878c2d52abf9e7dbf9023
nakatomi4614 <nakatomi4614@gmail.com>
2020-08-26 21:23:26 +0900

    条件文

--- python_chapter3.py
+++ python_chapter3.py
@@ -1,15 +1,73 @@
-#　一行目のコメントは入力省略しました
+#条件文
 
-#論理演算子 結果はbool
+"""
+もし　おなかすいている　なら:
+    ご飯を食べる
+そうでなければ:
+    ご飯は食べない
+"""
 
-print(1 == 1 and 2 == 2)
-print(1 == 2 and 2 == 2)
-print(2 == 1 and 2 == 2)
-print(1 == 1 and 10 != 2 and 2 < 10)
-print(1 == 1 or 2 == 2)
-print(1 == 1 or 1 == 2)
-print(1 == 1 or 2 == 1)
-print(1 == 2 or 2 == 1)
-print(1 == 2 or 2 == 1 or 1 == 1)
-print(not 1 == 1)
-print(not 1 == 2)
+home = "アメリカ"
+if home == "アメリカ":
+    print("Hello, America!")
+else:
+    print("Hello, World!")
+
+home = "日本"
+if home == "アメリカ":
+    print("Hello, America!")
+else:
+    print("Hello, World!")
+
+x = 2
+if x == 2:
+    print("数値は２です")
+if x % 2 == 0:
+    print("偶数です")
+if x % 2 != 0:
+    print("奇数です！")
+
+x = 10
+y = 11
+if x == 10:
+    if y == 11:
+        print(x+y)
+
+home = "タイ"
+if home == "日本":
+    print("Hello, Japan!")
+elif home == "タイ":
+    print("Hello, Thailand!")
+elif home == "インド":
+    print("hello, India!")
+elif home == "中国":
+    print("hello, China!")
+else:
+    print("Hello, World!")
+
+home = "タイ"
+if home == "日本":
+    print("Hello, Japan!")
+elif home == "南極":
+    print("Hello, Thailand!")
+elif home == "インド":
+    print("hello, India!")
+elif home == "中国":
+    print("hello, China!")
+else:
+    print("Hello, World!")
+
+x = 100
+if x == 10:
+    print("10")
+elif x == 20:
+    print("20")
+else:
+    print("わからないです")
+if x == 100:
+    print("100")
+
+if x % 2== 0:
+    print("偶数")
+else:
+    print("奇数")
