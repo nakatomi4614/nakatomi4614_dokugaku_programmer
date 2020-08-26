544264e6a47de6aadace84b5c9f7baa17e2bacd3
nakatomi4614 <nakatomi4614@gmail.com>
2020-08-26 21:07:59 +0900

    論理演算子

--- python_chapter3.py
+++ python_chapter3.py
@@ -1,17 +1,15 @@
 #　一行目のコメントは入力省略しました
 
-#比較演算子 結果はbool
+#論理演算子 結果はbool
 
-A = 13
-B = 5
-
-print(A > B) #True
-print(A < B) #False
-print(2>=2)
-print(2<=2)
-print(2==2)
-print(1==2)
-print(1!=2) #not equalは!=
-print(2!=2)
-
-
+print(1 == 1 and 2 == 2)
+print(1 == 2 and 2 == 2)
+print(2 == 1 and 2 == 2)
+print(1 == 1 and 10 != 2 and 2 < 10)
+print(1 == 1 or 2 == 2)
+print(1 == 1 or 1 == 2)
+print(1 == 1 or 2 == 1)
+print(1 == 2 or 2 == 1)
+print(1 == 2 or 2 == 1 or 1 == 1)
+print(not 1 == 1)
+print(not 1 == 2)
