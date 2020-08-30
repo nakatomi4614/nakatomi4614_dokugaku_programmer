150b1cbaaa5f5912b32f627d94cf42c8971220e6
nakatomi4614 <nakatomi4614@gmail.com>
2020-08-30 19:02:44 +0900

    完成#https://atcoder.jp/contests/abc162/tasks/abc162_b

--- Atcoder_kaitou.py
+++ Atcoder_kaitou.py
@@ -1,9 +1,13 @@
 #https://atcoder.jp/contests/abc162/tasks/abc162_b
 #S_list = list(map(int, input("入力:").split()))
 S = int(input("n?:"))
-for i in range(S):
-    if i % 3 != 0 and i % 5!= 0 and i % 15 != 0:
+sum_all = 0
+sum_fizzbuzz = 0
+for i in range(1,S+1):
+    sum_all = sum_all + i
+    if i % 3 == 0 or i % 5 == 0 or i % 15 ==0:
+        sum_fizzbuzz = sum_fizzbuzz + i
+print(sum_all - sum_fizzbuzz)
 
-#print(result)
 
 
