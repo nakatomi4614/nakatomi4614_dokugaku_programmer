ea57ffede536e7c218ff6862ecb089de4416f8de
nakatomi4614 <nakatomi4614@gmail.com>
2020-09-03 21:13:26 +0900

    tupleまで終わり

--- python_chapter5.py
+++ python_chapter5.py
@@ -1,66 +1,66 @@
-#5章　コンテナ
-#メソッド
+# 5章　コンテナ
+# メソッド
+"""
+print("hello".upper( ))
+# 大文字にするメソッド
+print("Hello".replace("o", "@"))
+# 文字列を入れ替えるreplace
 
-print("hello".upper())
-#大文字にするメソッド
-print("Hello".replace("o","@"))
-#文字列を入れ替えるreplace
-
-#list
-#空リスト
-fruit = list()
+# list
+# 空リスト
+fruit = list( )
 print(fruit)
 fruit = ["Apple", "Orange", "Pear"]
 print(fruit)
-#追加　append
+# 追加　append
 fruit.append("Banana")
 fruit.append("Peach")
 print(fruit)
 
-#どんなobjectでも一つのlistに入る
-random = list()
+# どんなobjectでも一つのlistに入る
+random = list( )
 random.append(True)
 random.append(100)
 random.append(1.1)
 random.append("Hello")
 print(random)
 
-#list tapleは　イテラブルオブジェクト 繰り返し可能
-for i in range(3) :
+# list tapleは　イテラブルオブジェクト 繰り返し可能
+for i in range(3):
     print(fruit[i])
 try:
-    print(fruit[10]) #範囲外をしていしたらerror
+    print(fruit[10])  # 範囲外をしていしたらerror
 except IndexError as e:
     print(e)
 
-#listはミュータブル　変更可能です
+# listはミュータブル　変更可能です
 colors = ["blue", "green", "yellow"]
 print(colors)
-colors[2] = "red" #index2をredに入れ替え
+colors[2] = "red"  # index2をredに入れ替え
 print(colors)
 
-#listから末尾を取り除く　pop
-#popは空listには使えない
+# listから末尾を取り除く　pop
+# popは空listには使えない
 print(colors)
-item = colors.pop()
+item = colors.pop( )
 print(item)
 print(colors)
 
-#listの連結　+
+# listの連結　+
 colors1 = ["blue", "green", "yellow"]
 colors2 = ["orange", "pink", "black"]
-print(colors1+colors2)
+print(colors1 + colors2)
 
-#含まれているか調べる　調べたいもの　in　ソースリスト
-print("green" in colors) #boolを返す
+# 含まれているか調べる　調べたいもの　in　ソースリスト
+print("green" in colors)  # boolを返す
 
-#含まれていないことを調べる　not in
+# 含まれていないことを調べる　not in
 print("black" not in colors)
 
-#要素数　len
+# 要素数　len
 print(len(colors))
 
-#sample
+# sample
 colors = ["purple", "orange", "green"]
 guess = input("何色かな？：")
 
@@ -68,7 +68,34 @@ if guess in colors:
     print("大当たり！")
 else:
     print("ハズレやでぇ")
+"""
+#タプル
+#タプルはイミュータブル　変更不可　tuple
 
+my_tuple = tuple()
+print(my_tuple)
 
+rndm = ("M.Jackson", 1958, True)
+print(rndm)
 
+#一つの要素のtupleは,をつけて書く
+print(("一つだけのタプル", ))
+print((9)+1)
+print("上のは計算の（）と認識される")
+
+#tupleに追加すると例外エラー
+dys = ("1984","Brave New World")
+try:
+    dys[1] = "Handmaid's Tale"
+except TypeError as e:
+    print(e)
+#要素の取り出しはインデックスで位置を指定
+print(dys[1])
+#要素が含まれているかどうかはlistと同じようにin
+
+print("1983" in dys)
+print("1984" in dys)
+print("1984" not in dys)
+
+#tupleは要素を変更したくない場合やdictのキーに
 
