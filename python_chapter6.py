02778319ea05cd6a62f3ba407e03fc73a95955ea
nakatomi4614 <nakatomi4614@gmail.com>
2020-09-06 17:35:52 +0900

    スライスまで　6章完了

--- python_chapter6.py
+++ python_chapter6.py
@@ -65,11 +65,61 @@ print(string.split("。"))
 first_three = "abc"
 result = "+".join(first_three)
 print(result)
-"""
+
 # joinメソッドの引数にリストを渡す
 words = ["The", "fox", "jumped","over","the","fence","."]
 one = " ".join(words)
 print(one)
 #空白文字をリストの間に入れる
 
+#空白除去 strip
+s = "   あんドー  ナツ　　　　"
+print(s)
+print(s.strip())
+#.strip()は最初と最後の空白を取り除く 半角全角問わない
+
+#置換　replace
+equ = "All animals are equal."
+equ = equ.replace("a","@")
+print(equ)
+#aを@に置換している
+
+#文字を探す
+print(equ.index("n"))
+#ない場合はValueError
+try:
+    "animals".index("z")
+except ValueError as e:
+    print(e)
+
+#包含　in演算子で指定文字列が含まれているかどうか探す　戻り値はbool
+s = "Cat in the hat."
+print("Cat" in s)
+#含まれるのでTrue
+print("Catt" in s)
+#含まれないのでFalse
+#not in で含まれないことを確認できる
+print("Cat" not in s)
+#含まれているのでFalse
+#escape文字
+print("彼女は"そうだね"と言った。")
+"""
+print("彼女は \"そうだね\" と言った。")
+# \をエスケープ文字といい、特別な意味を持たせる。
+# https://www.javadrive.jp/python/string/index2.html
+print("一行目\n二\'行目\"")
+# \nは改行
+
+# スライス　一部分を取り出す[開始:終了]
+fict = ["トルストイ", "カミュ", "オーウェル", "ハクスリー", "オースティン"]
+print(fict[0:3]) #1番目から3番目まで表示　インデックス0,1,2を表示
+
+#文字のスライス
+ivan = "死の代わりに一つの光があった。"
+print(ivan[0:6])
+print(ivan[6:16])
+#開始indexが0の場合は省略可
+print(ivan[:6])
+#最後まで含む場合は終了インデックスを省略できる。
+print(ivan[6:])
 
