d60ef1c276079670eb8006f609eb7fd05d1a6fb1
nakatomi4614 <nakatomi4614@gmail.com>
2020-09-25 19:00:25 +0900

    pycamp3章途中まで

---
+++ pycamp_3.py
@@ -0,0 +1,16 @@
+#データ型
+#int 整数型
+print(1+2,2-4,6*9,10/3,5%2, 5**2,10//3)
+#float 浮動小数点
+print(5.0+5.2)
+#strは”or'でかこんだものどっちもいっしょ
+print("テスト", 'ﾃｽﾄ')
+#エスケープシーケンス
+print('I\'m Hiroki')
+print('Hello\nworld')
+print("I'm Hiroki") #ダブルクオートで囲めばシングルクオートが表示される
+#対話モードでは'foo\nbar\nbaz\n'
+"""foo
+bar
+baz
+"""
