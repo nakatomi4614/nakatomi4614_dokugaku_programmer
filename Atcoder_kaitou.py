cfffd91a4bb3627f1507f3831e9dd41a3bcef2f9
nakatomi4614 <nakatomi4614@gmail.com>
2020-10-16 15:58:04 +0900

    https://atcoder.jp/contests/abc124/tasks/abc124_b

--- Atcoder_kaitou.py
+++ Atcoder_kaitou.py
@@ -1,14 +1,10 @@
-#https://atcoder.jp/contests/abc129/tasks/abc129_b
+# https://atcoder.jp/contests/abc124/tasks/abc124_b
 n = int(input())
-S_list = list(map(int, input().split()))
-sum_all = sum(S_list)
-sum_1 = 0
-sum_list =[]
-for i in S_list:
-    sum_1 = sum_1 + i
-    sum_2 = sum_all - sum_1
-    sum_list.append(abs(sum_1-sum_2))
-print(min(sum_list))
-
-
-
+S_list = list(map(int, input( ).split( )))
+i = 1
+result_list = []
+for H_i in S_list:
+    if H_i >= max(S_list[:i]):
+        result_list.append(H_i)
+    i = i + 1
+print(len(result_list))
