08272566bee439c71f9d4e56a8cb246e980ccd83
nakatomi4614 <nakatomi4614@gmail.com>
2020-10-16 17:01:37 +0900

    完了　https://atcoder.jp/contests/abc114/tasks/abc114_b

--- Atcoder_kaitou.py
+++ Atcoder_kaitou.py
@@ -1,10 +1,7 @@
 # https://atcoder.jp/contests/abc114/tasks/abc114_b
-n = int(input())
-S_list = list(map(int, input( ).split( )))
-i = 1
+# S_list = list(map(int, input( ).split( )))
+n = input()
 result_list = []
-for H_i in S_list:
-    if H_i >= max(S_list[:i]):
-        result_list.append(H_i)
-    i = i + 1
-print(len(result_list))
+for i in range(len(n)):
+    result_list.append(abs(int(n[i:i+3])- 753))
+print(min(result_list))
