3e047521784247ebc07d3386bef02aec67e972bf
nakatomi4614 <nakatomi4614@gmail.com>
2020-10-16 15:41:12 +0900

    完了　https://atcoder.jp/contests/abc129/tasks/abc129_b

--- Atcoder_kaitou.py
+++ Atcoder_kaitou.py
@@ -1,5 +1,14 @@
 #https://atcoder.jp/contests/abc129/tasks/abc129_b
-n = int(input("n?:"))
-S_list = list(map(int, input("入力:").split()))
+n = int(input())
+S_list = list(map(int, input().split()))
+sum_all = sum(S_list)
+sum_1 = 0
+sum_list =[]
+for i in S_list:
+    sum_1 = sum_1 + i
+    sum_2 = sum_all - sum_1
+    sum_list.append(abs(sum_1-sum_2))
+print(min(sum_list))
+
 
 
